9411a0ffe1643a9002a2f9158c33a9f04dc0d1fd
Larry Joy <ljoy@microsoft.com>
2025-08-15 10:24:06 -0700

    Merge branch 'main' of https://github.com/microsoft/crescent-credentials into refactor-setup-scripts

--- clippy_command.sh
+++ clippy_command.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
+set -ex
 
-# Invoke clippy with this command to allow some lints
+ALLOW_FLAGS="
+  -A clippy::needless_range_loop
+  -A clippy::same_item_push
+  -A clippy::should_implement_trait
+  -A clippy::result_large_err
+"
 
-RUSTFLAGS="--deny warnings" cargo clippy --release --tests -- \
-    --no-deps \
-    -A clippy::needless_range_loop \
-    -A clippy::same_item_push \
-    -A clippy::should_implement_trait \
-    -A clippy::result_large_err
+# Extra rustc lint relaxations (empty by default)
+EXTRA_RUSTFLAGS=""
+
+case "$PWD" in
+  */ecdsa-pop|*/ecdsa-pop/*)
+    # dead_code is a rustc lint → must go into RUSTFLAGS
+    EXTRA_RUSTFLAGS="$EXTRA_RUSTFLAGS -Adead_code"
+    ;;
+esac
+
+# Deny all warnings globally, but allow dead_code in ecdsa-pop
+RUSTFLAGS="$EXTRA_RUSTFLAGS -Dwarnings" cargo clippy --release --tests -- \
+  --no-deps \
+  $ALLOW_FLAGS
